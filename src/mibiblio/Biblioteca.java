d9f89b48011a558b25d1f68f325070d79c3bd0d3
Samuel L.C <slcardoso.docente@gmail.com>
2023-03-07 11:27:28 +0100

    Upload from linux

---
+++ src/mibiblio/Biblioteca.java
@@ -0,0 +1,145 @@
+package mibiblio;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+
+/**
+ * Clase que representa a una <b>Biblioteca</b> que contiene un conjunto de libros.
+ * Los libros pueden estar repetidos o no:
+ * Actualmente un objeto biblioteca puede realizar las siguientes operaciones:
+ * <ol>
+ * <li>Agregar libros a la colección.</li>
+ * <li>Obtener el número total de libros (contando  o excluyendo repetidos</li>
+ * <li>Obtener libros buscando por alguno de sus autores</li>
+ * <li>Obtener libros buscando por su título o parte de su título.</li>
+ * <li>Obtener todos los autores para los que existe algún libro almacenado</li>
+ * </ol>
+ * @author Aquí tu nombre
+ * @see Libro
+ * @see Autor
+ */
+public class Biblioteca {
+    /**
+     * Capacidad máxima de ejemplares que pueden ser almacenados en la biblioteca.
+     */
+    public static int CAPACIDAD_MAX = 1000;
+    private List<Libro> libros;
+
+    /**
+     * Crea una biblioteca vacía.
+     */
+    public Biblioteca() {
+        libros = new ArrayList();
+    }
+    
+    /**
+     * Agrega un libro a la biblioteca. <br>
+     * Los libros se mantendrán ordenados alfabéticamente según su título.
+     * @param lib Libro a agregar.
+     * @return True si se agrega el libro y false si no se agrega por haber alcanzado
+     * la capacidad máxima.
+     * @see #CAPACIDAD_MAX
+     */
+    public boolean addLibro(Libro lib){
+        if(libros.size()==Biblioteca.CAPACIDAD_MAX)
+            return false;
+        
+        int i = 0;
+        for(i=0;i<libros.size();i++){
+            if(libros.get(i).getTitulo().compareTo(lib.getTitulo())>=0){
+                break;
+            }
+        }
+        libros.add(i,lib);
+        return true;
+    }
+    
+    /**
+     * Devuelve el número total de ejemplares almacenados. <br>
+     * Se incluyen repetidos.
+     * @return Número de libros almacenados.
+     */
+    public int getNumLibros(){
+        return libros.size();
+    }
+    
+    /**
+     * Devuelve el número de libros almacenados sin contar repetidos.
+     * @return Número de libros diferentes almacenados.
+     */
+    public int getNumTitulos(){
+        return (new HashSet(libros)).size();
+    }
+    
+    /**
+     * Devuelve una lista de libros almacenados escritos por el autor indicado.
+     * @param autor Autor por el que se desea filtrar.
+     * @return Libros de la biblioteca escritos por el autor.
+     */
+    public List<Libro> getLibrosPorAutor(Autor autor){
+        HashSet<Libro> libros = new HashSet();
+        for(Libro l:this.libros){
+            if(l.esDeAutor(autor)){
+                libros.add(l);
+            }
+        }
+        return new ArrayList(libros);
+    }
+    
+    /**
+     * Deveuelve una lista de libros filtrando por el título. <br>
+     * El título del libro debe contener todo parte del texto pasado como parámetro.
+     * @param titulo Filtro a aplicar sobre el título del libro.
+     * @return Conjunto de libros cuyo título contienen parte del texto indicado en el parámetro.
+     */
+    public List<Libro> getLibrosPorTitulo(String titulo){
+        HashSet<Libro> libros = new HashSet();
+        for(Libro l:this.libros){
+            if(l.getTitulo().toLowerCase().contains(titulo.toLowerCase())){
+                libros.add(l);
+            }
+        }
+        return new ArrayList(libros); 
+    } 
+    
+    /**
+     * Devuelve una lista ordenada alfabéticamente con todos los autores
+     * de los que se tiene algún libro almacenado.
+     * @return Lista de autores.
+     */
+    public List<Autor> getAutores(){
+        HashSet<Autor> autores = new HashSet();
+        for(Libro l:this.libros){
+            autores.addAll(l.getAutores());
+        }
+        List<Autor> ordenados = new ArrayList(autores);
+        //Los ordeno con un comparator
+        Comparator<Autor> cmp = (Autor a1, Autor a2) -> {            
+            String sA1 = a1.getApellidos().toLowerCase()+a1.getNombre();
+            String sA2 = a2.getApellidos()+a2.getNombre();
+            return sA1.compareToIgnoreCase(sA2);
+        };
+        
+        Collections.sort(ordenados, cmp);
+        return ordenados;        
+    }
+    
+
+    @Override
+    public String toString() {
+        String libros = "";
+        for(Libro l:this.libros){
+            libros+="-----------------------------------------\n";
+            libros+=l;
+            libros+="\n-----------------------------------------\n";
+        }
+        return libros;
+    }
+    
+    
+    
+    
+}
---
+++ src/mibiblio/Libro.java
@@ -0,0 +1,109 @@
+package mibiblio;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+
+
+public class Libro {
+    private String isbn;
+    private String titulo;
+    private List<Autor> autores;
+    private LocalDate publicacion;
+    private String editorial;
+
+    public Libro(String isbn, String titulo, List<Autor> autores, LocalDate publicacion, String editorial) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autores = autores;
+        this.publicacion = publicacion;
+        this.editorial = editorial;
+    }
+
+    public boolean esDeAutor(Autor autor){
+        return autores.contains(autor);
+    }
+    
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public List<Autor> getAutores() {
+        return autores;
+    }
+
+    public void setAutores(List<Autor> autores) {
+        this.autores = autores;
+    }
+
+
+    public LocalDate getPublicacion() {
+        return publicacion;
+    }
+
+    public void setPublicacion(LocalDate publicacion) {
+        this.publicacion = publicacion;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 83 * hash + Objects.hashCode(this.isbn);
+
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Libro other = (Libro) obj;
+        
+        return Objects.equals(this.isbn, other.isbn);
+    }
+
+    @Override
+    public String toString() {
+        String autores="";
+        for(Autor a : this.autores){
+            autores += "\t"+a+"\n";
+        }
+        if(autores.equals(""))
+            autores = "\tAnónimo\n";
+        return titulo+"\n"+autores+editorial+" ("+publicacion.getYear()+")\n"+isbn;
+    }
+    
+   
+    
+    
+
+    
+    
+}
